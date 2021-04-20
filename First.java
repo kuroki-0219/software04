7456b6cc6cca3d3bd678c9dbfa8527496a80a940
kuroki-0219 <75403490+kuroki-0219@users.noreply.github.com>
2021-04-20 21:26:49 +0900

    Add files via upload

---
+++ First.java
@@ -0,0 +1,6 @@
+public class First{
+  public static void main(String[] args){
+    System.out.println("好きな食べ物は唐揚げです。");
+    System.out.println("私が生まれた大分県では唐揚げが有名で、小さい頃から何度も大分の美味しい唐揚げを食べてきたからです。");
+  }
+}
